3a2b5c8089582e4c7b4d6d54a62882979e4ada0d
NGUYEN HOAI THUONG <ngthuong.io@gmail.com>
2021-10-22 10:25:15 +0700

    add base test case of app pages

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from pages.views import HomePageView
 
-# Create your tests here.
+
+class HomepageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('homepage')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'pages/homepage.html')
+
+    def test_homepage_contains_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(response, 'Hi there! I should not be on the page.')
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
